6a47d5fbcd951e247bc4dd367e684bab6bdd7afc
alex <alex@mail.ru>
2019-04-18 00:34:24 +0500

    stable

--- main.cpp
+++ main.cpp
@@ -20,16 +20,14 @@ struct logging_allocator {
     pointer free_memory_ptr = nullptr;
 
 
-    logging_allocator()
-    {
-        std::cout << __PRETTY_FUNCTION__ << "[n = " << byte_reserved << "]" << std::endl;
+    logging_allocator() {
+        //std::cout << __PRETTY_FUNCTION__ << "[n = " << byte_reserved << "]" << std::endl;
         reserved_memory_ptr = reinterpret_cast<T *>(std::malloc(byte_reserved));
         free_memory_ptr = reserved_memory_ptr;
 
     }
-    ~logging_allocator()
-    {
-        std::cout << __PRETTY_FUNCTION__ << "[n = " << byte_reserved << "]" << std::endl;
+    ~logging_allocator() {
+        //std::cout << __PRETTY_FUNCTION__ << "[n = " << byte_reserved << "]" << std::endl;
         std::free(reserved_memory_ptr);
     }
 
@@ -55,14 +53,15 @@ struct logging_allocator {
     }
 
     void destroy(T *p) {
-        p->~T();
+
+        if(p != nullptr)
+            p->~T();
     }
 };
 
 
 template<typename T, typename U>
-struct MapNode
-{
+struct MapNode {
     using key_type = T;
     using value_type = U;
 
@@ -83,66 +82,115 @@ struct MapNode
         return _value;
     }
 
-    MapNode& getNext() {
+    MapNode* getNext() {
         return _nextElement;
     }
 
-    void setNext(MapNode& next_) {
+    void setNext(MapNode* next_) {
         _nextElement = next_;
     }
 };
 
 template<typename T, typename U, typename Allocator>
-struct myMap
-{
+struct myMap {
     using key_type = T;
-
     using value_type = U;
-
     typedef typename Allocator::template rebind<MapNode<T,U>>::other MyMapAllocator;
 
     MyMapAllocator _allocator;
+    MapNode<T,U>* _headElement;
+    MapNode<T,U>* _tailElement;
+    size_t _size = 0;
 
-    MapNode<T,U>* headElement;
-
-    void add(const key_type key_, value_type value_)
-    {
-        MapNode<T,U>* temp = _allocator.allocate(48);
-        _allocator.construct(temp, key_, value_);
-        headElement = temp;
+    ~myMap() {
+        auto temp = _headElement;
+        for(size_t i = 0; i < _size; i++) {
+            auto temp1 = temp->getNext();
+            _allocator.destroy(temp);
+            temp = temp1;
+        }
+        _allocator.deallocate(_headElement, _size);
     }
 
-    value_type get(const key_type key_)
-    {
-        headElement->getValue();
+
+    void add(const key_type key_, value_type value_) {
+        MapNode<T,U>* temp = _allocator.allocate(1);
+        _allocator.construct(temp, key_, value_);
+
+        if(_size == 0)
+            _headElement = temp;
+        else
+            _tailElement->setNext(temp);
+
+        _tailElement = temp;
+        _size++;
+    }
+
+    value_type get(const key_type key_) {
+
+        auto temp = _headElement;
+        for(size_t i = 0; i < _size; i++) {
+
+            if(temp->getKey() == key_)
+                return temp->getValue();
+
+            temp = temp->getNext();
+        }
+        return _headElement->getValue();
     }
 
 };
 
-int main(int, char *[]) {
-
-    /*
+void f1() {
     //std map and std allocator
+    std::cout << " === std::map and std::allocator ===" << std::endl;
     auto m = std::map<int, int, std::less<int>>{};
+
     for (size_t i = 0; i < 10; ++i) {
         m[i] = std::tgamma(i+1);
         std::cout << i << " " << m[i] << std::endl;
     }
+}
 
+void f2() {
     //std map and my allocator
+    std::cout << " === std::map and my allocator ===" << std::endl;
     auto n = std::map<int, int, std::less<int>, logging_allocator<std::pair<const int, int>, 10>>{};
+
     for (size_t i = 0; i < 10; ++i) {
         n[i] = std::tgamma(i+1);
         std::cout << i << " " << n[i] << std::endl;
     }
-*/
+}
 
-    auto map = myMap<const int, int, logging_allocator<std::pair<const int, int>, 10>>{};
+void f3() {
+    //my map and std::allocator
+    std::cout << " === my map and std::allocator ===" << std::endl;
+    auto r = myMap<int, int, std::allocator<MapNode<const int, int>>>{};
 
-    map.add(1,20);
-    std::cout << map.get(1) << std::endl;
+    for (size_t i = 0; i < 10; ++i) {
+        r.add(i, std::tgamma(i+1));
+        std::cout << i << " " << r.get(i) << std::endl;
+    }
+}
 
+void f4() {
+    //my map and my allocator
+    std::cout << " === my map and my allocator ===" << std::endl;
+    auto p = myMap<int, int, logging_allocator<std::pair<const int, int>, 10>>{};
 
+    for (size_t i = 0; i < 10; ++i) {
+        p.add(i, std::tgamma(i+1));
+        std::cout << i << " " << p.get(i) << std::endl;
+    }
+}
+
+int main(int, char *[]) {
+
+    f1();
+    f2();
+    f3();
+    f4();
 
     return 0;
 }
